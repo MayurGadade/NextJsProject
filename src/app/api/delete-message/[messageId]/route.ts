a6674b234b61c14e4ed947ad8de91fbc96d5f88d
MayurGadade <mayurgadade3011@gmail.com>
2025-03-31 18:35:02 +0530

    navbar and messageCard created and backend in delete api created

---
+++ src/app/api/delete-message/[messageId]/route.ts
@@ -0,0 +1,46 @@
+import UserModel from "@/model/User";
+import dbConnect from "@/lib/dbConnect";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import { getServerSession } from "next-auth/next";
+import { User } from "next-auth";
+import mongoose from "mongoose";
+
+export async function DELETE(req: Request, {params}: {params:{messageId:string}}) {
+    const messageid= params.messageId;
+    await dbConnect();
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user as User;
+
+    if(!session || !user){
+
+        return Response.json({
+            success: false,
+            message: "Not Authorized"
+        },
+        { status: 401 }
+        )
+    }
+    try {
+        const updatedResult= await UserModel.findOneAndUpdate(
+            {_id: user._id},
+            {$pull:{messages:{_id: messageid}}}, 
+        )
+        if(updatedResult.modifiedCount==0){
+            return Response.json({
+                success: false,
+                message: "Message not found"
+            },{status:404})
+            return Response.json({
+                success:true,
+                message:"Message deleted successfully"
+            },{status:200})
+
+        }
+    } catch (error) {
+        console.log("error deleting message",error);
+        return Response.json({
+            success: false,
+            message: "Error deleting message"
+        },{status:500})
+    }
+}
---
+++ src/components/Messagecard.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "./ui/button";
+import { X } from "lucide-react";
+import { Message } from "@/model/User";
+import { toast } from "sonner";
+import axios from "axios";
+
+type MessagecardProps = {
+  message: Message;
+  onMessageDelete: (messageId: string) => void;
+};
+
+const Messagecard = ({ message, onMessageDelete }: MessagecardProps) => {
+  const handleDeleteConfirmetions = async () => {
+    axios.delete(`/api/delete-message/${message._id}`);
+    toast.success("Message deleted successfully", {
+      description: "Message deleted successfully",
+    });
+    onMessageDelete(message._id as string);
+  };
+
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant="destructive">
+                <X className="h-5 w-5" />
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  your account and remove your data from our servers.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={() => handleDeleteConfirmetions}>
+                  Continue
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent></CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default Messagecard;
